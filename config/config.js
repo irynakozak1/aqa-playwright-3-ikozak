668281e6d419ba58220aa2babddb4c293f822a95
irynakozak <iryna.kozak.qa@gmail.com>
2024-04-09 14:58:54 +0300

    homework 20. PW ENV variables

---
+++ config/config.js
@@ -0,0 +1,10 @@
+
+const config = {
+    baseUrl: process.env.BASE_URL,
+    httpCredentials: {
+        username: process.env.HTTP_CREDENTIALS_USERNAME,
+        password: process.env.HTTP_CREDENTIALS_PASSWORD,
+    }
+}
+
+export default config
